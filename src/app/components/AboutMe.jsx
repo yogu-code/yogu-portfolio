f058d21f5a1c68e6cceba09c42d3e567631bce35
yogu-code <yogeshrane019@gmail.com>
2025-06-05 21:45:08 +0530

    fix: update image source path in AboutMe component

--- src/app/components/AboutMe.jsx
+++ src/app/components/AboutMe.jsx
@@ -62,7 +62,7 @@ export default function AboutMe() {
       {/* Right Side - Image */}
       <div className="order-2 flex-1 flex justify-center items-center w-full md:w-auto">
         <Image
-          src="./portfolio/MySelf.jpeg"
+          src="./MySelf.jpeg"
           alt="Yogesh Rane"
           width={400}
           height={400}
