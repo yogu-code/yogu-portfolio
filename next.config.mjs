50fdcf0faf880dffcdd93ccf38dcab3efe71de75
yogu-code <yogeshrane019@gmail.com>
2025-06-20 21:39:41 +0530

    refactor: remove basePath and assetPrefix configurations from nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-export const nextConfig = {
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-  images: {
-    unoptimized: true, // Optional for GitHub Pages, as it doesn't support Next.js image optimization
-  },
-};
-
+const nextConfig = {};
+export default nextConfig;
