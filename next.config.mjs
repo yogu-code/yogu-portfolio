7749bab6646a0373e755beb451a430af783e0606
yogu-code <yogeshrane019@gmail.com>
2025-06-20 22:36:03 +0530

    feat: add image configuration to nextConfig for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+};
 export default nextConfig;
