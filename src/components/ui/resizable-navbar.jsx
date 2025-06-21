bb79498dbe25d4e939e3b8cae32d50f4b3e7811a
yogu-code <yogeshrane019@gmail.com>
2025-06-21 12:54:51 +0530

    chore: clean up unused imports and improve code formatting in resizable-navbar component

--- src/components/ui/resizable-navbar.jsx
+++ src/components/ui/resizable-navbar.jsx
@@ -194,11 +194,6 @@ export const NavbarLogo = () => {
     <a
       href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black">
-      <img
-        src="/images/terminal.png"
-        alt="logo"
-        width={30}
-        height={30} />
       <span className="font-medium text-black dark:text-white">Yogu-Code</span>
     </a>
   );
